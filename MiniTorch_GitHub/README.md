59d6f569eed0201c53bcff98ad0c70cbca245e4c
Зорькина Лиза <59500744+zorkina@users.noreply.github.com>
2026-09-23 13:01:23 +0300

    Delete MiniTorch_GitHub directory

--- MiniTorch_GitHub/README.md
+++
@@ -1,5 +0,0 @@
-# MiniTorch Modules 0–1
-
-Implementation of MiniTorch Modules 0 and 1.
-
-Tests are run with GitHub Actions.
--- MiniTorch_GitHub/minitorch/__init__.py
+++
@@ -1,8 +0,0 @@
-from . import operators
-from .autodiff import Context, Variable, backpropagate, central_difference, topological_sort
-from .module import Module, Parameter
-from .scalar import Scalar, ScalarHistory, derivative_check
-from .scalar_functions import ScalarFunction
-from .optim import Optimizer, SGD
-from .datasets import Graph, datasets
-from .testing import MathTest, MathTestVariable
